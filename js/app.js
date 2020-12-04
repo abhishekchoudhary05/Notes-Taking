70b12967d375b3c34121bd539166bf2445a600c8
Choudhary, Abhishek <achoudha@itron.com>
2020-12-05 01:05:16 +0530

    completed Application

--- js/app.js
+++ js/app.js
@@ -12,9 +12,9 @@ button.addEventListener("click", function(ele){
     else{
         lStorage = JSON.parse(nodes);
     }
-    console.log(lStorage);
+    // console.log(lStorage);
     lStorage.push(txt.value);
-    console.log(lStorage);
+    // console.log(lStorage);
     window.localStorage.setItem("notes", JSON.stringify(lStorage));
     txt.value = "";
     ShowAllNotes();
@@ -37,11 +37,48 @@ function ShowAllNotes(){
             <div class="card-body">
                 <h5 class="card-title">${index+1}</h5>
                 <p class="card-text">${element}</p>
-                <a href="#" class="btn btn-primary">Delete Note</a>
+                <a index="${index}" onclick="deleteNote(this.index)" class="btn btn-primary">Delete Note</a>
             </div>
             </div>        
         `;
     });
     let notes = window.document.getElementById("notes");
-    notes.innerHTML = html;
+    if(lStorage != 0 ){
+        notes.innerHTML = html;
+    }
+    else{
+        notes.innerHTML = `<b>There is no any notes to display. please create one.</b>`;
+    }
 }
+
+// function to delete note from localstorage
+function deleteNote(index){
+    // console.log("Deleting this note.");
+    let notes = window.localStorage.getItem('notes');
+    if(notes == null){
+        allNotes = [];
+    }
+    else{
+        allNotes = JSON.parse(notes);
+    }
+    allNotes.splice(index, 1);
+    window.localStorage.setItem('notes', JSON.stringify(allNotes));
+    ShowAllNotes();
+}
+
+let search = window.document.getElementById('searchTxt');
+search.addEventListener('input', function(){
+    // getting all cards
+    let allElement = window.document.getElementsByClassName('noteCard');
+    
+    // for each card check weather inserted text is there in it or not    
+    Array.from(allElement).forEach(function(element){
+        let text = element.getElementsByTagName('p')[0].innerText;
+        if(text.includes(search.value)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";            
+        }
+    });
+});
